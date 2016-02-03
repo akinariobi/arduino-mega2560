bd6ded2c2f08cca3b59305e8be6584141064d4b1
Shcherbakova Iuliia <yuliyanasher@gmail.com>
2016-02-03 22:12:59 +0100

    GAS sensor with serial monitor.ino
    blablabla

---
+++ GAS sensor with serial monitor.ino
@@ -0,0 +1,17 @@
+/* Testing MQ-2 GAS sensor with serial monitor
+   Suitable for detecting of LPG, i-butane, propane, methane ,alcohol, Hydrogen or smoke
+   More info: http://www.ardumotive.com/how-to-use-mq2-gas-sensor-en.html                    */
+
+const int gasPin = A0; //GAS sensor output pin to Arduino analog A0 pin
+
+
+void setup()
+{
+	Serial.begin(9600); //Initialize serial port - 9600 bps
+}
+
+void loop()
+{
+	Serial.println(analogRead(gasPin));
+	delay(1000); // Print value every 1 sec.
+}
